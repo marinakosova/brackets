2680db88bcfbf0265dbfa1efd77384709249a58a
marinakosova <marina.kosova@gmail.com>
2020-10-23 12:14:51 -0500

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    obj = Object.fromEntries(bracketsConfig);
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let elem = str[i];
+        let lastStackElement = stack[stack.length - 1]
+        if (elem === obj[lastStackElement]) {
+            stack.pop();
+        } else if (obj[elem]) {
+            stack.push(elem);
+        } else {
+            return false;
+        }
+    }
+
+    if (stack.length == 0)
+        return true;
+    else
+        return false;
 }
